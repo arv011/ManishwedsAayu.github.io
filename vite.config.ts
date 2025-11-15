6b90035df884451b3b4e1553f39e2d0709f5c0d2
arvind <arvindshrivastav1919@gmail.com>
2025-11-15 16:52:41 +0530

    reverted

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,22 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// for github
-export default defineConfig({
-  plugins: [react()],
-  base: "/ManishwedsAayu.github.io"
-})
+// for g
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/ManishwedsAayu.github.io"
+// })
 // https://vitejs.dev/config/
 
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
